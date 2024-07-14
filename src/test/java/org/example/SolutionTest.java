49a0c22e53ba919b2f326303159eebdefbee91c6
Alvin <alvinngg9@gmail.com>
2024-07-13 17:25:41 -0700

    add tests

--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -11,6 +11,12 @@ public class SolutionTest {
         Assert.assertEquals(4, solution.search(new int[]{4,5,6,7,0,1,2}, 0));
         Assert.assertEquals(-1, solution.search(new int[]{4,5,6,7,0,1,2}, 3));
         Assert.assertEquals(-1, solution.search(new int[]{1}, 0));
+        Assert.assertEquals(4, solution.search(new int[]{4,5,6,7,8,1,2,3}, 8));
+        Assert.assertEquals(0, solution.search(new int[]{5,1,3}, 5));
+        Assert.assertEquals(0, solution.search(new int[]{6,7,1,2,3,4,5}, 6));
+        Assert.assertEquals(4, solution.search(new int[]{4,7,9,1,2,3}, 2));
+        Assert.assertEquals(-1, solution.search(new int[]{6,7,1,2,3,4,5}, 0));
+        Assert.assertEquals(1, solution.search(new int[]{6,7,1,2,3,4,5}, 7));
     }
 
 }
